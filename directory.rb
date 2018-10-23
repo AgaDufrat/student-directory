862e0fa66e4cd732451b994a15494511da448859
Aga <a.dufrat@gmail.com>
2018-10-23 14:48:51 +0100

    Print names shorter that 12

--- directory.rb
+++ directory.rb
@@ -17,10 +17,8 @@ def print_header
 end
 def print(students)
   students.each_with_index do |student, index|
-    if student[:name][0] == "A" || "a" # name beginning with A
-      if student[:name][0].length < 12 # name shorter that 12
+    if student[:name][0].length < 12 # name shorter that 12
       puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-      end
     end
   end
 end
