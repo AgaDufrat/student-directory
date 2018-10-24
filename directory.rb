ea2e5a7a79246722f7049333080607285cbfdbe8
Aga <a.dufrat@gmail.com>
2018-10-24 10:17:25 +0100

    Ask for cohort and validate input

--- directory.rb
+++ directory.rb
@@ -1,22 +1,27 @@
 
 def input_students
   students = []
+  cohorts = [:january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december]
   puts "Please enter the names of the students and their details"
-  puts "To finish, just hit return twice"
+  puts "To finish, just hit return twice  when you are asked for a name"
   name = gets.chomp
-  puts "Enter their date of birth"
-  dob = gets.chomp
-  puts "Enter their country of birth"
-  country = gets.chomp
+  puts "Enter their cohort"
+  cohort = gets.chomp
   while !name.empty? do
-    students << {name: name, dob: dob, country: country, cohort: :november}
+    # provide default value if empty
+    cohort = :novmeber if cohort.empty?
+    # check for typos
+    while !cohorts.include? cohort
+      puts "Looks like you made a typo, try again"
+      cohort = gets.chomp.to_sym
+    end
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
     puts "Enter the name of the student"
     name = gets.chomp
-    puts "Enter their date of birth"
-    dob = gets.chomp
-    puts "Enter their country of birth"
-    country = gets.chomp
+    puts "Enter their cohort"
+    cohort = gets.chomp.to_sym
+
   end
   students
 end
@@ -27,7 +32,7 @@ end
 def print(students)
   i = 0
   while i < students.length do
-    puts "#{i + 1}. #{students[i][:name]} born on #{students[i][:dob]} in #{students[i][:country]} (#{students[i][:cohort]} cohort)".center(200)
+    puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)"
     i +=1
   end
 end
