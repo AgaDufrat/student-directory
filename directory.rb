f46ae58956e987f7301e9596326a944d592df5c0
Aga <a.dufrat@gmail.com>
2018-10-26 14:46:34 +0100

    Use a code block (do...end) to access a file

--- directory.rb
+++ directory.rb
@@ -85,15 +85,13 @@ end
 def save_students
   puts "Enter filename"
   filename = STDIN.gets.chomp
-  # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
+  File.open(filename, "w") do |file|
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
   end
-  file.close
 end
 
 def try_load_students
